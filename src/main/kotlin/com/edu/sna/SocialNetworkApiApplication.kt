dd9afd5928b995f4dd50ae8d3f7de695187ec85b
a.tsitsinov <tsitsinov.anton@gmail.com>
2020-12-25 23:38:22 +0300

    initial

---
+++ src/main/kotlin/com/edu/sna/SocialNetworkApiApplication.kt
@@ -0,0 +1,11 @@
+package com.edu.sna
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialNetworkApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialNetworkApiApplication>(*args)
+}
